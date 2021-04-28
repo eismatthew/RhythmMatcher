b4aff4af1fabc83da0c78a168274b9beef71de58
Matthew Eis <eismatthewd@gmail.com>
2021-04-28 14:52:38 -0700

    finish initial styling. work on game/animation logic

---
+++ src/song.js
@@ -0,0 +1,46 @@
+var a = {
+  color: 'rgba(228, 19, 60, 1)',
+  next: 0,
+  notes: [
+    { duration: 3, delay: 4.2 },
+  ]
+};
+
+var s = {
+  color: 'rgba(28, 228, 88, 1)',
+  next: 0,
+  notes: [
+    { duration: 3, delay: 2.2 },
+    { duration: 3, delay: 6.2 },
+  ]
+};
+
+var k = {
+  color: 'rgba(36, 33, 236, 1)',
+  next: 0,
+  notes: [
+    { duration: 3, delay: 0.2 },
+    { duration: 3, delay: 8.2 },
+ 
+  ]
+};
+
+
+var l = {
+  color: 'rgba(230, 245, 22, 1)',
+  next: 0,
+  notes: [
+    { duration: 3, delay: 3.2 },
+    { duration: 3, delay: 3.7 },
+    { duration: 3, delay: 11 },
+    { duration: 3, delay: 12 },
+    { duration: 3, delay: 14.1 },
+  ]
+};
+
+
+
+var song = {
+  duration: 212,
+  sheet: [a, s, k, l]
+};
