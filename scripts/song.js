3b269fbd4a7ec51290061943fe6470b64b564468
Matthew Eis <eismatthewd@gmail.com>
2021-04-30 14:57:42 -0700

    continue with note timing and patterns

--- scripts/song.js
+++ scripts/song.js
@@ -18,23 +18,18 @@ var a = {
   next: 0,
   notes: [
     { duration: 3.33, delay: 0 }, //1
-    { duration: 3.33, delay: 1.55 }, //3
-    // { duration: 3.33, delay: 3.1}, //5
-    // { duration: 3.33, delay: 4.65 }, //7
-    { duration: 3.33, delay: 6.2}, //9
-    { duration: 3.33, delay: 7.75 }, //13
-    { duration: 3.33, delay: 9.3 }, //17
-    { duration: 3.33, delay: 10.85}, //21
-    { duration: 3.33, delay: 12.4}, //25
-     { duration: 3.33, delay: 0 + two_bar }, //1
-    { duration: 3.33, delay: 1.55 + two_bar  }, //3
-    { duration: 3.33, delay: 3.1 + two_bar }, //5
-    { duration: 3.33, delay: 4.65 + two_bar  }, //7
-    { duration: 3.33, delay: 6.2 + two_bar }, //9
-    // { duration: 3.33, delay: 7.75 + two_bar  }, //13
-    // { duration: 3.33, delay: 9.3 + two_bar  }, //17
-    // { duration: 3.33, delay: 10.85 + two_bar }, //21
-    { duration: 3.33, delay: 12.4 + two_bar }, //25
+    { duration: 3.33, delay: 1.55 }, //2
+    { duration: 3.33, delay: 6.2}, //3
+    { duration: 3.33, delay: 7.75 }, //4
+    { duration: 3.33, delay: 9.3 }, //5
+    { duration: 3.33, delay: 10.85}, //6
+    { duration: 3.33, delay: 12.4}, //7
+    { duration: 3.33, delay: 0 + two_bar }, //8
+    { duration: 3.33, delay: 1.55 + two_bar  }, //9
+    { duration: 3.33, delay: 3.1 + two_bar }, //10
+    { duration: 3.33, delay: 4.65 + two_bar  }, //11
+    { duration: 3.33, delay: 6.2 + two_bar }, //12
+    { duration: 3.33, delay: 12.4 + two_bar }, //13
 
     //break
     { duration: 3.33, delay: 0 + four_bar}, 
@@ -48,23 +43,34 @@ var s = {
   color: 'rgba(15, 157, 88, 1)',
   next: 0,
   notes: [
-    { duration: 3.33, delay: 3.1}, //5
-    { duration: 3.33, delay: 4.65 }, //7
+    { duration: 3.33, delay: 3.1}, //1
+    { duration: 3.33, delay: 4.65 }, //2
     // { duration: 3.33, delay: 6.2 + two_beat}, //11
     // { duration: 3.33, delay: 7.75 + two_beat }, //15
     // { duration: 3.33, delay: 9.3 + two_beat }, //19
     // { duration: 3.33, delay: 10.85 + two_beat}, //23
     // { duration: 3.33, delay: 12.4 + two_beat}, //27
-    { duration: 3.33, delay: 6.2 + two_beat + two_bar }, //11
-    { duration: 3.33, delay: 7.75 + two_beat + two_bar  }, //15
+
+    { duration: 3.33, delay: 6.2 + two_beat}, //10
+    { duration: 3.33, delay: 7.75 + two_beat }, //14
+
+
+    { duration: 3.33, delay: 0 + four_bar - eight_beat}, 
+    // { duration: 3.33, delay: 6.2 + four_bar - five_beat + one_bar  }, //11
+    // { duration: 3.33, delay: 7.75 + four_bar - five_beat + one_bar }, //15
+
+  
+  
+
+    
     // { duration: 3.33, delay: 9.3 + two_beat  + two_bar }, //19
     // { duration: 3.33, delay: 10.85 + two_beat + two_bar }, //23
     // { duration: 3.33, delay: 12.4 + two_beat + two_bar }, //27
-    // { duration: 3.33, delay: 7.75 + two_bar  }, //13
-    // { duration: 3.33, delay: 9.3 + two_bar   }, //17
+    // // { duration: 3.33, delay: 7.75 + two_bar  }, //13
+    // // { duration: 3.33, delay: 9.3 + two_bar   }, //17
     
-    //break
-    { duration: 3.33, delay: 3.1 + four_bar},
+    // //break
+    // { duration: 3.33, delay: 3.1 + four_bar},
 
   ]
 };
@@ -77,17 +83,16 @@ var k = {
     { duration: 3.33, delay: 1.55  + two_beat}, //4
     { duration: 3.33, delay: 3.1  + two_beat}, //6
     { duration: 3.33, delay: 4.65  + two_beat}, //8
-    { duration: 3.33, delay: 6.2 + two_beat}, //10
-    { duration: 3.33, delay: 7.75 + two_beat }, //14
+    // { duration: 3.33, delay: 6.2 + two_beat}, //10
+    // { duration: 3.33, delay: 7.75 + two_beat }, //14
     { duration: 3.33, delay: 9.3 + two_beat }, //18
     { duration: 3.33, delay: 10.85 + two_beat}, //22
-    { duration: 3.33, delay: 12.4 + two_beat}, //26
-    // { duration: 3.33, delay: 0  + two_beat + two_bar }, //2
-    { duration: 3.33, delay: 1.55  + two_beat + two_bar }, //4
+    // { duration: 3.33, delay: 12.4 + two_beat}, //26
+    // { duration: 3.33, delay: 1.55  + two_beat + two_bar }, //4
     { duration: 3.33, delay: 3.1  + two_beat + two_bar }, //6
     { duration: 3.33, delay: 4.65 + two_beat + two_bar }, //8
-    { duration: 3.33, delay: 6.2 + two_beat + two_bar }, //10
-    { duration: 3.33, delay: 7.75 + two_beat + two_bar }, //14
+    // { duration: 3.33, delay: 6.2 + two_beat + two_bar }, //10
+    // { duration: 3.33, delay: 7.75 + two_beat + two_bar }, //14
     // { duration: 3.33, delay: 9.3 + one_beat  + two_bar }, //18
     // { duration: 3.33, delay: 10.85 + one_beat + two_bar }, //22
     // { duration: 3.33, delay: 12.4 + one_beat + two_bar }, //26
@@ -104,10 +109,10 @@ var l = {
   color: 'rgba(244, 180, 0, 1)',
   next: 0,
   notes: [
-    { duration: 3.33, delay: 0 + one_beat }, 
-    { duration: 3.33, delay: 1.55 + one_beat }, 
-    { duration: 3.33, delay: 0 + five_beat }, 
-    { duration: 3.33, delay: 1.55 + five_beat }, 
+    // { duration: 3.33, delay: 0 + one_beat }, 
+    // { duration: 3.33, delay: 1.55 + one_beat }, 
+    // { duration: 3.33, delay: 0 + five_beat }, 
+    // { duration: 3.33, delay: 1.55 + five_beat }, 
 
 
 
@@ -116,8 +121,25 @@ var l = {
 
 
 
-    { duration: 3.33, delay: 9.3 + four_bar + two_bar}, 
-    { duration: 3.33, delay: 10.85 + two_bar}, 
+    // { duration: 3.33, delay: 9.3 + four_bar + two_bar}, 
+    // { duration: 3.33, delay: 10.85 + four_bar + two_bar}, 
+
+
+
+     { duration: 3.33, delay: 0  + one_beat}, //2
+    { duration: 3.33, delay: 1.55  + one_beat}, //4
+    { duration: 3.33, delay: 3.1  + one_beat}, //6
+    { duration: 3.33, delay: 4.65  + one_beat}, //8
+    { duration: 3.33, delay: 6.2 + one_beat}, //10
+    { duration: 3.33, delay: 7.75 + one_beat }, //14
+    { duration: 3.33, delay: 9.3 + one_beat }, //18
+    { duration: 3.33, delay: 10.85 + one_beat}, //22
+    { duration: 3.33, delay: 12.4 + one_beat}, //26
+   
+    
+
+    //break
+    { duration: 3.33, delay: 4.65 + two_beat + two_bar - eight_beat }, //8
   ]
 };
 
